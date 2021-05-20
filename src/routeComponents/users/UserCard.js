12401bccba29d6fe00a86cb7222abab8af9a46f1
Camila Boaventura <camilamboaventura@gmail.com>
2021-05-20 14:09:38 +0200

    Styles added to UserDetail

--- src/routeComponents/users/UserCard.js
+++ src/routeComponents/users/UserCard.js
@@ -4,16 +4,16 @@ import "../patients/UsersCard.css";
 function UserCard(props) {
   return (
     <Link
-      className="text-decoration-none "
+      className="text-decoration-none user-card"
       key={props.user._id}
       to={`/users/${props.user._id}`}
     >
       <div
-        className="card card-fixed-height text-dark shadow rounded border-0 m-2"
+        className="card card-fixed-height text-dark shadow rounded border-0 m-2 "
         style={{ width: "100%" }}
       >
         <img
-          className="card-img product-img mx-auto mt-2"
+          className="card-img product-img mx-auto mt-2 card-pic"
           src={props.user.user_pic}
           alt="user"
         />
@@ -22,12 +22,14 @@ function UserCard(props) {
             <small>{props.user.name}</small>
           </h4>
 
-          <p className="mb-0">
-            <small className="card-text">{new Date(props.user.date_of_birth).toLocaleDateString()}</small>
-          </p>
+          {/* <p className="mb-0">
+            <small className="card-text">
+              {new Date(props.user.date_of_birth).toLocaleDateString()}
+            </small>
+          </p> */}
 
-          <p className="card-text mb-0">
-            <small>{props.user.gender}</small>
+          <p className="mb-0">
+            <small className="card-text">ID: {props.user._id}</small>
           </p>
 
           <p className="card-text mb-0">
--- src/routeComponents/users/UserDetail.js
+++ src/routeComponents/users/UserDetail.js
@@ -1,9 +1,10 @@
 import { useState, useEffect, useContext } from "react";
 import { useParams, Link, useHistory } from "react-router-dom";
-
+import "../../assets/styles/UserDetail.css";
 import api from "../../apis/api";
 import { AuthContext } from "../../contexts/authContext";
 import ConfirmationModal from "../../components/ConfirmationModal";
+import NavbarLogged from "../../components/NavBarLogged";
 
 function UserDetail() {
   const [state, setState] = useState({
@@ -46,82 +47,108 @@ function UserDetail() {
   }, [id]);
 
   return (
-    <div>
-      {loggedInUser.user.role === "ADMIN" ? (
-        <div className="row d-flex justify-content-end">
-          <Link to={`/users/edit/${id}`} className="btn btn-warning mr-3">
-            Edit
-          </Link>
-          {/* Abrimos um modal de confirmação antes de deletar o produto */}
-          <button className="btn btn-danger" onClick={() => setShowModal(true)}>
-            Delete
-          </button>
+    <div className="allContent">
+      <NavbarLogged />
+      <div className="container">
+        {loggedInUser.user.role === "ADMIN" ? (
+          <div className="row d-flex justify-content-end">
+            <Link
+              to={`/users/edit/${id}`}
+              className="btn btn-outline-success buttonConfig"
+            >
+              Edit
+            </Link>
+            {/* Abrimos um modal de confirmação antes de deletar o produto */}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => setShowModal(true)}
+            >
+              Delete
+            </button>
+          </div>
+        ) : null}
+        <div className="d-flex justify-content-start align-items-center profile-box">
+          <div className="col img-profile">
+            <img
+              className="card-img product-img mx-auto mt-2 photo rounded-circle"
+              src={state.user_pic}
+              alt="user"
+            />
+          </div>
+          <div className="col ml-5">
+            <div className="card-body">
+              <h3 className="card-title">
+                <strong>{state.name}</strong>
+              </h3>
+
+              <p className="card-text mb-0">
+                <strong>Social Security Number: </strong>
+                {state.social_security_number}
+              </p>
+
+              <p className="mb-0">
+                <strong>Gender:</strong> {state.gender}
+              </p>
+
+              <p className="mb-0">
+                <strong>Role:</strong> {state.role}
+              </p>
+              {state.role === "DOCTOR" ? (
+                <p className="mb-0">
+                  <strong>Medical Specialty:</strong> {state.medical_specialty}
+                </p>
+              ) : null}
+
+              <p>
+                <strong>Date of Birthday: </strong>
+                {new Date(state.date_of_birth).toLocaleDateString()}
+              </p>
+            </div>
+          </div>
         </div>
-      ) : null}
 
-    
+        <div className="d-flex  align-items-center adressBox mt-4">
+          <div className="col addressPatient">
+            <h2>Address Info</h2>
 
-      <img
-        className="card-img product-img mx-auto mt-2"
-        src={state.user_pic}
-        alt="user"
-      />
-      <div className="card-body">
-        <h4 className="card-title">
-          <small>{state.name}</small>
-        </h4>
+            <ul>
+              <li>
+                <strong>Post Code: </strong>
+                {state.address.postCode}
+              </li>
+              <li>
+                <strong>Street: </strong>
+                {state.address.street}
+              </li>
+              <li>
+                <strong>Neighbourhood: </strong>
+                {state.address.neighbourhood}
+              </li>
+              <li>
+                <strong>City: </strong>
+                {state.address.city}
+              </li>
+              <li>
+                <strong>State or Province: </strong>
+                {state.address.stateOrProvince}
+              </li>
+              <li>
+                <strong>Country: </strong>
+                {state.address.country}
+              </li>
+            </ul>
+          </div>
+        </div>
 
-        <p className="card-text">{state.social_security_number}</p>
-
-        <p className="mb-0">{state.gender}</p>
-
-        <p className="mb-0">{state.role}</p>
-        <p className="mb-0">{state.medical_specialty}</p>
-
-        <p>
-          <small>
-            Date of Birthday: {new Date(state.date_of_birth).toLocaleDateString()}
-          </small>
-        </p>
-
-        <h2>Address Info</h2>
-        <hr />
-
-        <ul>
-          <li>
-            <strong>Post Code: </strong>
-            {state.address.postCode}
-          </li>
-          <li>
-            <strong>Street: </strong>
-            {state.address.street}
-          </li>
-          <li>
-            <strong>Neighbourhood: </strong>
-            {state.address.neighbourhood}
-          </li>
-          <li>
-            <strong>City: </strong>
-            {state.address.city}
-          </li>
-          <li>
-            <strong>State or Province: </strong>
-            {state.address.stateOrProvince}
-          </li>
-          <li>
-            <strong>Country: </strong>
-            {state.address.country}
-          </li>
-        </ul>
+        <ConfirmationModal
+          show={showModal}
+          handleClose={() => setShowModal(false)}
+          handleConfirm={() => history.push(`/users/delete/${id}`)}
+          title="Are you sure you want to delete this product?"
+        >
+          <p>This action is irreversible. To confirm, click "Confirm".</p>
+        </ConfirmationModal>
       </div>
-      <ConfirmationModal
-        show={showModal}
-        handleClose={() => setShowModal(false)}
-        handleConfirm={() => history.push(`/users/delete/${id}`)}
-        title="Are you sure you want to delete this product?"
-      >
-        <p>This action is irreversible. To confirm, click "Confirm".</p>
-      </ConfirmationModal>
     </div>
   );
 }
